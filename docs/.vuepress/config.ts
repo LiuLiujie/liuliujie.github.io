fc7e52f4d7004f95d90966bea2aea92761272e2b
Yujie Liu <yujie.liu.home@gmail.com>
2022-11-18 23:52:33 +0100

    [feat] add title, description and line number for markdown code

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "vuepress/config";
 import nav from "./nav/nav";
 
 export default defineConfig({
+    title: "Googuy's blog",
+    description: "Googuy's blog",
     themeConfig:{
         nav: nav,
         sidebarDepth: 2,
         repo: "https://github.com/LiuLiujie/liuliujie.github.io"
+    },
+    markdown: {
+        lineNumbers: true
     }
 });
