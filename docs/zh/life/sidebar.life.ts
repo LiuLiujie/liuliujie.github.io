7c2c9116ce00ba4a95726d0c34a026409ece81a4
Yujie <yujie.liu.public@gmail.com>
2022-12-23 22:43:45 +0100

    [life][diary] 2022-12-20 - 2022-12-23

--- docs/zh/life/sidebar.life.ts
+++ docs/zh/life/sidebar.life.ts
@@ -3,12 +3,9 @@ import {SidebarConfigArray} from "@vuepress/theme-default/lib/shared/nav";
 
 export const sidebarLife4ZH: SidebarConfigArray = [
     'README.md',
+    '日记',
     {
-        text: "荷兰留学",
-        collapsible: true,
-        children: []
-    },{
-        text: "日记",
+        text: "我的随笔",
         collapsible: true,
         children: []
     }
