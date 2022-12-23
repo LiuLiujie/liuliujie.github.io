06129693a408e25b92de45f7da6cec389c3894ef
Yujie <yujie.liu.public@gmail.com>
2022-12-23 22:46:24 +0100

    [life][diary] 2022-12-20 - 2022-12-23

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -4,7 +4,7 @@ import {sidebarProjects} from "../projects/sidebar.projects";
 import {sidebarStudy4ZH} from "../zh/study/sidebar.study";
 import {sidebarProjects4ZH} from "../zh/projects/sidebar.projects";
 import {sidebarLife} from "../life/sidebar.life";
-import {sidebarLife4ZH} from "../zh/aboutme/sidebar.life";
+import {sidebarLife4ZH} from "../zh/life/sidebar.life";
 
 export const Sidebar4EN: SidebarConfig = {
 
