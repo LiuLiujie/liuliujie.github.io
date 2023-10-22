d46083309197b4c0eda0f62e43546a198ec87e35
Yujie <yujie.liu.public@gmail.com>
2023-10-22 10:50:36 +0200

    [feat] add category

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -5,6 +5,7 @@ import { lifeNavbar } from "../life/life-navbar";
 
 export const navbarConfig = navbar([
   { text: "Home", link: '/'},
+  { text: "Category", link: '/category/'},
   { text: "Articles", prefix: '/article/', children: articleNavbar},
   { text: "DevOps", prefix: '/article/devops/', children: devopsNavbar},
   { text: "Projects", prefix:'/projects/', children: projectNavbar},
