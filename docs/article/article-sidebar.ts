48b5d31e25284fcd5cc8467caa39dbba7b11ddff
Yujie <yujie.liu.public@gmail.com>
2023-11-24 22:14:44 +0100

    [blogs] static, inher and poly

--- docs/article/article-sidebar.ts
+++ docs/article/article-sidebar.ts
@@ -5,18 +5,29 @@ export const javaSideBar = [
         text: "一、Java 基础",
         collapsible: true,
         children:[
-            {text: "1.1 面向对象基础", link:"oop-basic.md"},
-            "interface.md"
+            {text: "面向对象基础", link:"oop-basic.md"},
+            {text: "Java 实例方法，静态方法与抽象方法"},
+            {text: "Java 继承和多态", link:"java-inheritance-polymorphism.md"},
+            {text: "Java 方法重写和重载"},
+            {text: "Java 抽象类"},
+            {text: "Java 接口", link: "interface.md"},
+            {text: "Java 内部类"},
+            {text: "Java 可变参数方法"},
+            {text: "Java finally 关键字", link: "finally.md"},
+            {text: "Java this 和 super 关键字"},
+            {text: "Java static 关键字", link: "static.md"},
+            {text: "Java final 关键字"},
+            {text: "Java instanceof 关键字"}
         ]
     },{
         text: "二、Java IO",
         prefix: "JavaIO/",
         collapsible: true,
         children:[
-            {text: "2.1 Java IO 体系简介",link:"README.md"},
-            {text: "2.2 Java 传统IO (BIO)", link: "java-io.md"},
-            {text: "2.3 Java NIO", link: "java-nio.md"},
-            {text: "2.4 Java AIO", link: "java-aio.md"}
+            {text: "Java IO 体系简介",link:"README.md"},
+            {text: "Java 传统IO (BIO)", link: "java-io.md"},
+            {text: "Java NIO", link: "java-nio.md"},
+            {text: "Java AIO", link: "java-aio.md"}
         ]
     },{
         text: "三、Java Collection (集合框架)",
