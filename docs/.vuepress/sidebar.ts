d64e3b60af699d5e3da405d083344dbea8f6c94b
Yujie <yujie.liu.public@gmail.com>
2023-11-24 22:43:31 +0100

    [feat] update sidebar

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -5,21 +5,36 @@ export const sidebarConfig = sidebar({
     "/article/":[
         { text: "All articles", link:""},
         {
-            text: "Java",
-            prefix:"java/",
-            collapsible: true,
+            text: "计算机基础",
+            children:[]
+        },
+        {
+            text: "前端",
             children:[
 
             ]
         },
         {
+            text: "后端",
+            children: [
+                "java/"
+            ]
+        },
+        {
             text: "DevOps",
             prefix:"devops/",
-            collapsible: true,
             children: [
 
             ]
-        }
+        },
+        {
+            text: "高性能计算",
+            prefix: "gpgpu/",
+            children:[
+                {text: "CUDA 教程", link: "cuda-tutorial-chinese/"},
+                {text: "OpenMPI", link: "MPI.md"}
+            ]
+        },
     ],
     "/article/java/": javaSideBar
 });
--- docs/article/article-sidebar.ts
+++ docs/article/article-sidebar.ts
@@ -2,25 +2,31 @@ export const javaSideBar = [
     { text: "<<返回所有文章", link: "/article/"},
     { text: "《Java学习路线(长期更新)》", link: "/article/java/"},
     {
-        text: "一、Java 基础",
+        text: "Java 基础",
         collapsible: true,
         children:[
             {text: "面向对象基础", link:"oop-basic.md"},
-            {text: "Java 实例方法，静态方法与抽象方法"},
+            //{text: "Java 实例方法，静态方法与抽象方法"},
             {text: "Java 继承和多态", link:"java-inheritance-polymorphism.md"},
-            {text: "Java 方法重写和重载"},
+            //{text: "Java 方法重写和重载"},
             {text: "Java 抽象类", link: "java-abstract-class.md"},
             {text: "Java 接口", link: "interface.md"},
-            {text: "Java 内部类"},
-            {text: "Java 可变参数方法"},
+            //{text: "Java 内部类"},
+            //{text: "Java 可变参数方法"},
             {text: "Java finally 关键字", link: "finally.md"},
-            {text: "Java this 和 super 关键字"},
+            //{text: "Java this 和 super 关键字"},
             {text: "Java static 关键字", link: "static.md"},
-            {text: "Java final 关键字"},
+            //{text: "Java final 关键字"},
             {text: "Java instanceof 关键字", link :"java-instanceof.md"}
         ]
     },{
-        text: "二、Java IO",
+        text: "Java 字符串",
+        collapsible: true,
+        children:[
+            
+        ]
+    },{
+        text: "Java IO",
         prefix: "JavaIO/",
         collapsible: true,
         children:[
@@ -30,19 +36,19 @@ export const javaSideBar = [
             {text: "Java AIO", link: "java-aio.md"}
         ]
     },{
-        text: "三、Java Collection (集合框架)",
+        text: "Java Collection (集合框架)",
         collapsible: true,
         children:[
             "java-collection.md"
         ]
     },{
-        text: "四、Java Concurrency (并发编程)",
+        text: "Java Concurrency (并发编程)",
         collapsible: true,
         children:[
             "java-concurrency.md"
         ]
     },{
-        text: "五、Java 反射与面向切面编程",
+        text: "Java 反射与面向切面编程",
         collapsible: true,
         children:[
 
